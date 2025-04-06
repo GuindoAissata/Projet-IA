fef63c32ff4c65b21f73daec1b12ff0966a8cde0
Aissata GUINDO <153300440+GuindoAissata@users.noreply.github.com>
2025-04-06 17:11:45 +0200

    Update main.py
    mise à jour

--- main.py
+++ main.py
@@ -1,26 +1,56 @@
 import tkinter as tk
+from tkinter import ttk
 import random
 from fonctions import creer_population,fitness,selection,crossover,mutation,formation
 from genetique_algo import genetique
 
-def dessiner_villes():
+villes= []
+
+
+def generer_villes():
     canvas.delete("all") #reinitialisé la fenetre 
+    global villes
     villes= []
     n = int(entry.get())
     for _ in range (n) :
-        x,y = random.randint(20,480), random.randint(20,480)
-        canvas.create_oval(x-5, y-5, x+5, y+5, fill= "red")
+        x,y = random.randint(10,490), random.randint(10,190)
+        canvas.create_oval(x-5, y-5, x+5, y+5, fill= "red",tags="ville")
+
         villes.append((x,y))
 
     for i in range (len(villes)-1):
         x1,y1 = villes[i]
         x2,y2 = villes [i+1]
-        canvas.create_line(x1,y1,x2,y2, fill ="black")#liaison entre les villes
+        canvas.create_line(x1,y1,x2,y2, fill ="black",width=2,tags="ligne")#liaison entre les villes
 
-    return villes #on utilisera la ville retournée pour paramètre de créer_population
+def dessiner_villes(villes):
+    canvas_resultat.delete("all") #reinitialisé la fenetre 
+    for i in range (len(villes)-1):
+        x1,y1 = villes[i]
+        x2,y2 = villes [i+1]
+        canvas_resultat.create_oval(x1-5, y1-5, x1+5, y1+5, fill= "red",tags="ligne")
+        canvas_resultat.create_line(x1,y1,x2,y2, fill ="black",width=2, tags = "ligne",)#liaison entre les villes
+    
+
+    x1, y1 = villes[-1]
+    x2, y2 = villes[0]
+    canvas_resultat.create_line(x1, y1, x2, y2, fill="red", width=2, tags="ligne")
+    canvas_resultat.create_oval(x1-5, y1-5, x1+5, y1+5, fill= "red",tags="ligne")
+
+
+    #return villes #on utilisera la ville retournée pour paramètre de créer_population
+
 
 def run_algo():
-    return best_way ,best_fit = algo_genetique()
+    taille_pop = 100
+
+
+    best_way ,best_fit = genetique(villes,taille_pop)
+    print(f"le meilleur chemin trouvé est : {best_way} ")
+    print(f"la meilleure distance trouvée est : {best_fit} ")
+
+
+    return best_way, best_fit ,dessiner_villes(best_way), label_distance.config(text=f"BEST DIST : {best_fit :.2f}")
 
 
 
@@ -58,20 +88,53 @@ print (f"la meilleure distance trouvée {best_fit}")
 
 # fenetre 
 fenetre = tk.Tk()
-fenetre.title("Voyageur de Commerce")
+fenetre.title("Travelling Salesman Problem")
+fenetre.geometry("600x700") #largeur x Hauteur
 
-entry = tk.Entry(fenetre) #utilisateur peut entrer une valeur 
+###############"generer les villes"##################
+frame_genere_ville = tk.Frame(fenetre , bg ="white" )
+frame_genere_ville.pack(side=tk.TOP, fill = tk.BOTH , expand= True)
+
+label1 = tk.Label(frame_genere_ville, text="CITY GENERATION", bg="white", font=("Arial", 14))
+label1.pack(pady=10)
+
+#Canva pour afficher les villes générées 
+canvas = tk.Canvas(frame_genere_ville,width=500,height=200,bg="white")
+canvas.pack()
+
+#################Résultat de l'algorithme###########""
+frame_genetic = tk.Frame(fenetre,bg = "white")
+frame_genetic.pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+
+label2 = tk.Label(frame_genetic, text="OUTPUT OF THE ALGORITHME", bg="white", font=("Arial", 14))
+label2.pack(pady=10)
+
+#Canvas pour afficher le résultat de l'algorithme
+canvas_resultat = tk.Canvas(frame_genetic, width=500, height=200, bg="white")
+canvas_resultat.pack()
+
+####################les boutons##################""
+#frame_boutton = tk.Frame(fenetre,bg="white")
+#frame_boutton.pack(side=tk.RIGHT, fill = tk.Y)
+
+#label3 = tk.Label(frame_boutton, text="Contrôles", bg="#f0f0f0", font=("Arial", 14))
+#label3.pack(pady=10)
+
+entry = tk.Entry(frame_genere_ville) #utilisateur peut entrer une valeur 
 entry.pack()
 
-bouton1= tk.Button(fenetre, text = "générer villes", command = dessiner_villes)
-bouton1.pack()
+#Boutons
 
-bouton2 = tk.Button(fenetre,text="Run the genetic algorithme", command=run_algo)
-bouton2.pack()
+bouton1= tk.Button(frame_genere_ville, text = "generate cities", command = generer_villes)
+bouton1.pack(pady=5)
+
+bouton2 = tk.Button(frame_genetic,text="Run the genetic algorithme", command=run_algo)
+bouton2.pack(pady=5)
+
+label_distance = tk.Label(frame_genetic,text="BEST DISTANCE : ")
+label_distance.pack()
 
 
-canvas = tk.Canvas(fenetre, width=500, height=500, bg="white")
-canvas.pack()
 
 
 
